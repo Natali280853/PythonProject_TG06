8c5e82377385a6dbc0e2054bfaab7441037aaab9
Natali280853 <dgorby2000@gmail.com>
2025-01-08 19:01:19 +0500

    clear comments

--- bot.py
+++ bot.py
@@ -3,17 +3,14 @@ import random
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import TOKEN
 import sqlite3
-import aiohttp
 import logging
 import requests
 
--- config.py
+++ config.py
@@ -1,2 +1,1 @@
 TOKEN = ''
-RAPIDAPI_KEY = ''
